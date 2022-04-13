8890ed59d8fb8ab7b406bb4d69fe483099d75366
ollo69 <oliveroandrea@libero.it>
2022-04-14 01:57:14 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,32 +7,40 @@ https://github.com/pypa/sampleproject
 """
 
 # Always prefer setuptools over distutils
+from os.path import dirname, join
+import re
 from setuptools import find_packages, setup
-from pyasuswrt import __version__
 
-VERSION = __version__
+LIB = "pyasuswrt"
+install_requires = ["aiohttp>=3.7.4"]
+github_url = "https://github.com/ollo69/pyasuswrt"
+
+with open(join(dirname(__file__), LIB, "__init__.py"), "r") as fp:
+    for line in fp:
+        m = re.search(r'^\s*__version__\s*=\s*([\'"])([^\'"]+)\1\s*$', line)
+        if m:
+            version = m.group(2)
+            break
+    else:
+        raise RuntimeError("Unable to find own __version__ string")
 
 with open("README.md", "r") as f:
     readme = f.read()
 
-install_requires = ["aiohttp >= 3.7.4"]
-
-github_url = "https://github.com/ollo69/pyasuswrt"
-
 setup(
     name="pyasuswrt",
-    version=VERSION,
+    version=version,
     description="Api wrapper for Asuswrt https://www.asus.com/ASUSWRT/ using protocol HTTP",
     long_description=readme,
     long_description_content_type="text/markdown",
     keywords=["asuswrt", "asuswrt wrapper"],
     url=github_url,
-    download_url=f"{github_url}/archive/{VERSION}.tar.gz",
+    download_url=f"{github_url}/archive/{version}.tar.gz",
     license="MIT",
     author="ollo69",
     author_email="ollo69@users.noreply.github.com",
     packages=find_packages(exclude=["contrib", "docs", "tests"]),
-    python_requires=">= 3.7",
+    python_requires=">=3.7",
     install_requires=install_requires,
     extras_require={},
     classifiers=[
